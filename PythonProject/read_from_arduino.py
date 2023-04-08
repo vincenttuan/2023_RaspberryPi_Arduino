001010e60cc8daa7940c008f01fb4b97793ae4a2
VincentTuan <vincentjava@yahoo.com.tw>
2023-04-08 16:26:26 +0800

    Add files via upload

--- PythonProject/read_from_arduino.py
+++ PythonProject/read_from_arduino.py
@@ -1,9 +1,21 @@
+import time
+
 import serial
 
 
 if __name__ == '__main__':
     ser = serial.Serial('COM5', 115200)
     while True:
-        if ser.in_waiting > 0:
-            data = ser.readline().decode().rstrip()
-            print("收到 arduino 的資料:", data)
+        try:
+            if ser.in_waiting > 0:
+                data = ser.readline().decode().rstrip()
+                print("收到 arduino 的資料:", data)
+        except Exception as e:
+            print('斷線~請檢查 USB 線')
+            time.sleep(3)
+            # 重新連線
+            try:
+                ser = serial.Serial('COM5', 115200)
+            except:
+                pass
+
