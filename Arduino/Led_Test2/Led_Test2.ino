ccdbc45dbd2b282900a49b6708931900cf7ad8b4
VincentTuan <vincentjava@yahoo.com.tw>
2023-04-08 12:10:21 +0800

    Add files via upload

---
+++ Arduino/Led_Test2/Led_Test2.ino
@@ -0,0 +1,48 @@
+// 控制站
+const int RELAY_PIN_1 = 8;
+const int RELAY_PIN_2 = 9;
+const int RELAY_PIN_3 = 11;
+const int RELAY_PIN_4 = 12;
+const int SLEEP = 500;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(RELAY_PIN_1, OUTPUT);
+  pinMode(RELAY_PIN_2, OUTPUT);
+  pinMode(RELAY_PIN_3, OUTPUT);
+  pinMode(RELAY_PIN_4, OUTPUT);
+}
+
+void loop() {
+  if(Serial.available() > 0) {
+    char c = Serial.read();
+    Serial.print(c);  // 電腦顯示
+    switch(c){
+      case '1':
+        digitalWrite(RELAY_PIN_1, HIGH);
+        digitalWrite(RELAY_PIN_2, LOW);
+        digitalWrite(RELAY_PIN_3, LOW);
+        digitalWrite(RELAY_PIN_4, LOW);
+        break;
+      case '2':
+        digitalWrite(RELAY_PIN_1, LOW);
+        digitalWrite(RELAY_PIN_2, HIGH);
+        digitalWrite(RELAY_PIN_3, LOW);
+        digitalWrite(RELAY_PIN_4, LOW);
+        break;
+      case '3':
+        digitalWrite(RELAY_PIN_1, LOW);
+        digitalWrite(RELAY_PIN_2, LOW);
+        digitalWrite(RELAY_PIN_3, HIGH);
+        digitalWrite(RELAY_PIN_4, LOW);
+        break;
+      case '4':
+        digitalWrite(RELAY_PIN_1, LOW);
+        digitalWrite(RELAY_PIN_2, LOW);
+        digitalWrite(RELAY_PIN_3, LOW);
+        digitalWrite(RELAY_PIN_4, HIGH);
+        break;
+    }
+  }
+  
+}
