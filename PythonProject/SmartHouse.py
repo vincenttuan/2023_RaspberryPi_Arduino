bdb936fdc157710954a88fe359be8bb2101b1c7a
VincentTuan <vincentjava@yahoo.com.tw>
2023-04-15 16:12:11 +0800

    Add files via upload

--- PythonProject/SmartHouse.py
+++ PythonProject/SmartHouse.py
@@ -11,7 +11,32 @@ GUI
 
 import tkinter
 import threading
+import time
+import serial
 from tkinter import font
+
+def rev_data_from_arduino():
+    ser = serial.Serial('COM8', 115200)
+    time.sleep(1)
+    while True:
+        try:
+            if ser.in_waiting > 0:
+                data = ser.readline().decode().rstrip()
+                print("收到 arduino 的資料:", data)
+                dataArray = data.split(",")
+                data_value.set(dataArray[0])
+                temp_value.set(dataArray[1] + '°C')
+                humd_value.set(dataArray[2] + '%')
+
+
+        except Exception as e:
+            print('斷線~請檢查 USB 線')
+            time.sleep(3)
+            # 重新連線
+            try:
+                ser = serial.Serial('COM5', 115200)
+            except:
+                pass
 
 if __name__ == '__main__':
 
@@ -56,6 +81,10 @@ if __name__ == '__main__':
     led2_button.grid(row=2, column=1, sticky='EWNS')
     led3_button.grid(row=2, column=2, sticky='EWNS')
     led4_button.grid(row=2, column=3, sticky='EWNS')
+
+    # 啟動一條執行緒去執行
+    t1 = threading.Thread(target=rev_data_from_arduino)
+    t1.start()
 
     win.mainloop()
 
