56cf51dcf8365936686e21503097b70a7a7d0fd4
VincentTuan <vincentjava@yahoo.com.tw>
2023-04-22 14:27:48 +0800

    Add files via upload

--- PythonProject/SmartHouse.py
+++ PythonProject/SmartHouse.py
@@ -53,6 +53,28 @@ def rev_data_from_arduino():
                 data_value.set(dataArray[0])
                 temp_value.set(dataArray[1] + '°C')
                 humd_value.set(dataArray[2] + '%')
+                # LED/Relay Control
+                if dataArray[3] == '1':
+                    led1_value.set('LED1\nON')
+                    led2_value.set('LED2')
+                    led3_value.set('LED3')
+                    led4_value.set('LED4')
+                elif dataArray[4] == '1':
+                    led1_value.set('LED1')
+                    led2_value.set('LED2\nON')
+                    led3_value.set('LED3')
+                    led4_value.set('LED4')
+                elif dataArray[5] == '1':
+                    led1_value.set('LED1')
+                    led2_value.set('LED2')
+                    led3_value.set('LED3\nON')
+                    led4_value.set('LED4')
+                elif dataArray[6] == '1':
+                    led1_value.set('LED1')
+                    led2_value.set('LED2')
+                    led3_value.set('LED3')
+                    led4_value.set('LED4\nON')
+
 
 
         except Exception as e:
@@ -65,8 +87,6 @@ def rev_data_from_arduino():
                 pass
 
 if __name__ == '__main__':
-
-
     win = tkinter.Tk()
     win.title('Smart House')
     win.geometry('700x500')
@@ -79,6 +99,15 @@ if __name__ == '__main__':
     humd_value.set('0%')
     data_value = tkinter.StringVar()
     data_value.set('0')
+    led1_value = tkinter.StringVar()
+    led1_value.set('LED1')
+    led2_value = tkinter.StringVar()
+    led2_value.set('LED2')
+    led3_value = tkinter.StringVar()
+    led3_value.set('LED3')
+    led4_value = tkinter.StringVar()
+    led4_value.set('LED4')
+
     # view 元件
     temp_label = tkinter.Label(text='溫度', font=myfont)
     humd_label = tkinter.Label(text='濕度', font=myfont)
@@ -86,10 +115,10 @@ if __name__ == '__main__':
     humd_value_label = tkinter.Label(textvariable=humd_value, font=myfont)
     data_label = tkinter.Label(text='Data', font=myfont)
     data_value_label = tkinter.Label(textvariable=data_value, font=myfont)
-    led1_button = tkinter.Button(text='LED1', font=myfont, command=led1_click)
-    led2_button = tkinter.Button(text='LED2', font=myfont, command=led2_click)
-    led3_button = tkinter.Button(text='LED3', font=myfont, command=led3_click)
-    led4_button = tkinter.Button(text='LED4', font=myfont, command=led4_click)
+    led1_button = tkinter.Button(textvariable=led1_value, font=myfont, command=led1_click)
+    led2_button = tkinter.Button(textvariable=led2_value, font=myfont, command=led2_click)
+    led3_button = tkinter.Button(textvariable=led3_value, font=myfont, command=led3_click)
+    led4_button = tkinter.Button(textvariable=led4_value, font=myfont, command=led4_click)
 
     win.rowconfigure((0, 1, 2), weight=1)  # 列 0, 1, 2 同步放大與縮小
     win.columnconfigure((0, 1, 2, 3), weight=1)  # 欄 0, 1, 2, 3 同步放大與縮小
