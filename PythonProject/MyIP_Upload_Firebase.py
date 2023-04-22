06c7e901870a446c3035f8a38fdd68c765bf91fc
VincentTuan <vincentjava@yahoo.com.tw>
2023-04-22 10:37:40 +0800

    Add files via upload

---
+++ PythonProject/MyIP_Upload_Firebase.py
@@ -0,0 +1,14 @@
+import socket
+import requests
+import json
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8",80))
+ip = s.getsockname()[0]
+
+print(ip)
+
+firebase_url = 'https://iotfb-fc0b9.firebaseio.com/'
+data = {'ip':ip}
+result = requests.put(firebase_url + '/raspberry.json', verify=True, data=json.dumps(data))
+print(result)
