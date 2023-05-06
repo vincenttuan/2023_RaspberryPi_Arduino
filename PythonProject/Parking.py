f7fb99ad8f021fa0477eb08d6b566846483065d8
VincentTuan <vincentjava@yahoo.com.tw>
2023-05-06 11:14:14 +0800

    Add files via upload

---
+++ PythonProject/Parking.py
@@ -0,0 +1,114 @@
+import time
+import threading
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+from RPLCD.i2c import CharLCD
+
+# GPIO 設定 ------------------------------------------------------------
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)  # GPIO.BOARD, 使用BOARD模式
+
+# RFID 卡片前二碼 -------------------------------------------------------
+CARD_ID_1 = "55"
+CARD_ID_2 = "10"
+
+# 選擇控制伺服馬達的 PIN 腳 -----------------------------------------------
+servo1_pin = 16  # Servo 1
+servo2_pin = 18  # Servo 2
+GPIO.setup(servo1_pin, GPIO.OUT)
+GPIO.setup(servo2_pin, GPIO.OUT)
+# 設置 Servo PWM 頻率為 50Hz
+pwm1 = GPIO.PWM(servo1_pin, 50)
+pwm2 = GPIO.PWM(servo2_pin, 50)
+pwm1.start(0)
+pwm2.start(0)
+
+# 設定 LCD -------------------------------------------------------------
+reader = SimpleMFRC522()
+lcd = CharLCD('PCF8574', address=0x27, port=1, backlight_enabled=True)
+lcd.clear()  # LCD 清空
+
+# 設置蜂鳴器引腳和模式 ----------------------------------------------------
+buzzer_pin = 12  # 根據BOARD模式，將BCM 18對應的引腳更改為12
+GPIO.setup(buzzer_pin, GPIO.OUT)
+
+# 設定 Button ----------------------------------------------------------
+button_pin = 40
+GPIO.setup(button_pin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+
+
+def rfid_play():
+    try:
+        while True:
+            print("請將卡靠近讀卡器...")
+            GPIO.output(buzzer_pin, GPIO.LOW)
+            id, text = reader.read()
+            GPIO.output(buzzer_pin, GPIO.HIGH)
+            print("ID: %s\nText: %s" % (id, text))
+            leadId = str(id)[0:2]  # 取 RFID 的卡片 ID 前二碼
+            # 顯示在 LCD 螢幕上
+            lcd.cursor_pos = (0, 0)
+            lcd.write_string("ID:%s" % leadId)
+            time.sleep(0.1)
+            GPIO.output(buzzer_pin, GPIO.LOW)
+            time.sleep(0.1)  # 在讀取到卡片後等待一段時間
+
+            # 啟動 Servo
+            if leadId == CARD_ID_1:
+                servo1_play()
+            elif leadId == CARD_ID_2:
+                servo2_play()
+
+    finally:
+        GPIO.cleanup()
+
+
+def button_play():
+    while True:
+        time.sleep(0.5)
+        # 按下 button 將 LCD 螢幕清空
+        button_state = GPIO.input(button_pin)
+        # print(button_state)
+        if button_state == 0:
+            lcd.cursor_pos = (0, 0)
+            lcd.write_string("                ")
+
+# 設定 Servo 1 角度
+def set_angle1(angle):
+    duty_cycle = angle / 18 + 2
+    GPIO.output(servo1_pin, True)
+    pwm1.ChangeDutyCycle(duty_cycle)
+    time.sleep(1)
+    GPIO.output(servo1_pin, False)
+    pwm1.ChangeDutyCycle(0)
+
+# 設定 Servo 2 角度
+def set_angle2(angle):
+    duty_cycle = angle / 18 + 2
+    GPIO.output(servo2_pin, True)
+    pwm2.ChangeDutyCycle(duty_cycle)
+    time.sleep(1)
+    GPIO.output(servo2_pin, False)
+    pwm2.ChangeDutyCycle(0)
+
+def servo1_play():
+    # 旋轉伺服馬達
+    set_angle1(95)  # 開門
+    time.sleep(3)  # 停 3 秒
+    set_angle1(5)  # 關門
+
+def servo2_play():
+    # 旋轉伺服馬達
+    set_angle2(95)  # 開門
+    time.sleep(3)  # 停 3 秒
+    set_angle2(5)  # 關門
+
+if __name__ == '__main__':
+    set_angle1(5)  # Servo1 第一次先關門
+    set_angle2(5)  # Servo2 第一次先關門
+    t1 = threading.Thread(target=rfid_play)
+    t1.start()
+    t2 = threading.Thread(target=button_play)
+    t2.start()
+
+
